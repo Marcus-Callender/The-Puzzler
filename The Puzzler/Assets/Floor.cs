f0f5c016bf86d7ef40b7829f3602c5d2a73e871a
Twitchymini <zook.star@hotmail.co.uk>
2017-04-23 23:07:49 +0100

    floor colour

--- The Puzzler/Assets/Floor.cs
+++ The Puzzler/Assets/Floor.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Floor : MonoBehaviour {
+public class Floor : MonoBehaviour
+{
     Renderer m_material;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         m_material = gameObject.GetComponent<Renderer>();
         m_material.material.color = Color.blue;
-	}
+       
+    }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+        public static string materialcolour;
+
+
+}
 }

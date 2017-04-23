aeae5bdea1a94f5555b4ba4732bfafd0a68df4f6
Twitchymini <zook.star@hotmail.co.uk>
2017-04-23 21:57:43 +0100

    made vertical movements

--- The Puzzler/Assets/GameAssets/Code/Movement.cs
+++ The Puzzler/Assets/GameAssets/Code/Movement.cs
@@ -5,32 +5,47 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // how fast the charater moves left to right
-	float m_speed = 5.0f;
-=======
-	float m_speed = 10.0f;
->>>>>>> 7430715e068e35036461327a7883aa27fe560c69
+    float m_speed = 10.0f;
+    float m_jump = 5.0f;
 
-	void Start()
-	{
+    void Start()
+    {
 
-	}
-	
-	void Update()
-	{
-		if (Input.GetAxis("Horizontal") > 0.5f)
-		{
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0.5f)
+        {
             // gets a refrence to the position
-			Vector3 pos = gameObject.transform.position;
+            Vector3 pos = gameObject.transform.position;
             // adds to the position
             pos.x += m_speed * Time.deltaTime;
             // applies the new position
             gameObject.transform.position = pos;
-		}
-		else if (Input.GetAxis("Horizontal") < -0.5f)
-		{
-			Vector3 pos = gameObject.transform.position;
-			pos.x -= m_speed * Time.deltaTime;
-			gameObject.transform.position = pos;
-		}
-	}
+        }
+        else if (Input.GetAxis("Horizontal") < -0.5f)
+        {
+            Vector3 pos = gameObject.transform.position;
+            pos.x -= m_speed * Time.deltaTime;
+            gameObject.transform.position = pos;
+        }
+        if (Input.GetAxis("Vertical") > 0.5f)
+        {
+            // gets a refrence to the position
+            Vector3 pos = gameObject.transform.position;
+            // adds to the position
+            pos.y += m_jump * Time.deltaTime;
+            // applies the new position
+            gameObject.transform.position = pos;
+        }
+        else if (Input.GetAxis("Vertical") < -0.5f)
+        {
+            Vector3 pos = gameObject.transform.position;
+            pos.y -= m_jump * Time.deltaTime;
+            gameObject.transform.position = pos;
+        }
+    }
 }
+
+

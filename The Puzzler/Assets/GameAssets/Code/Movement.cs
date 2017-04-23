770dd4ad1e44f5627e0ce8741a47b3014c4c4611
Marcus Callender <m.shute@outlook.com>
2017-04-23 22:12:40 +0100

    Simple test jump

--- The Puzzler/Assets/GameAssets/Code/Movement.cs
+++ The Puzzler/Assets/GameAssets/Code/Movement.cs
@@ -6,7 +6,11 @@ public class Movement : MonoBehaviour
 {
     // how fast the charater moves left to right
     float m_speed = 10.0f;
-    float m_jump = 5.0f;
+    //float m_jump = 5.0f;
+    bool m_jumping = false;
+    float m_verticalSpeed = 0.0f;
+    float m_jumpForce = .100f;
+    float m_gravity = .0981f;
 
     void Start()
     {
@@ -32,17 +36,18 @@ public class Movement : MonoBehaviour
         }
         if (Input.GetAxis("Vertical") > 0.5f)
         {
-            // gets a refrence to the position
-            Vector3 pos = gameObject.transform.position;
-            // adds to the position
-            pos.y += m_jump * Time.deltaTime;
-            // applies the new position
-            gameObject.transform.position = pos;
+            if (!m_jumping)
+            {
+                m_jumping = true;
+                m_verticalSpeed = m_jumpForce;
+            }
         }
-        else if (Input.GetAxis("Vertical") < -0.5f)
+
+        if (m_jumping)
         {
             Vector3 pos = gameObject.transform.position;
-            pos.y -= m_jump * Time.deltaTime;
+            m_verticalSpeed -= m_gravity * Time.deltaTime;
+            pos.y += m_verticalSpeed;
             gameObject.transform.position = pos;
         }
     }

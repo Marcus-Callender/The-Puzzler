546e5fd81feb778c3b2838d8d104f1fb0948ffd9
Marcus Callender <m.shute@outlook.com>
2017-04-23 20:44:24 +0100

    Added movement to a cube

---
+++ The Puzzler/Assets/GameAssets/Code/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	float m_speed = 1.0f;
+
+	void Start()
+	{
+
+	}
+	
+	void Update()
+	{
+		if (Input.GetAxis("Horizontal") > 0.5f)
+		{
+			Vector3 pos = gameObject.transform.position;
+			pos.x += m_speed * Time.deltaTime;
+			gameObject.transform.position = pos;
+		}
+		else if (Input.GetAxis("Horizontal") < -0.5f)
+		{
+			Vector3 pos = gameObject.transform.position;
+			pos.x -= m_speed * Time.deltaTime;
+			gameObject.transform.position = pos;
+		}
+	}
+}

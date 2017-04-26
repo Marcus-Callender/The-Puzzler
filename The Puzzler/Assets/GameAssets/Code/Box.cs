5e8326d72fdecc54ae6812dfd9bef835edd0a38c
Marcus Callender <m.shute@outlook.com>
2017-04-26 13:57:05 +0100

    Revert "Reverted to d4c2f03"
    This reverts commit e9f63228d0ac60e86ae70c61e89dad85c8deb16b.

---
+++ The Puzzler/Assets/GameAssets/Code/Box.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.gray;
+    }
+    
+    void Update()
+    {
+
+    }
+}

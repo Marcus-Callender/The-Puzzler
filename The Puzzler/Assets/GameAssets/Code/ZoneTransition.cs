02eaebcba3d43356f79eeb6fa15768183a21a2ec
Marcus Callender <m.shute@outlook.com>
2017-05-01 19:20:31 +0100

    Added a door that changes colour which will be used for exiting the levels

---
+++ The Puzzler/Assets/GameAssets/Code/ZoneTransition.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneTransition : MonoBehaviour
+{
+    // 0 = red, 1 = green, 2 = blue
+    float[] m_colours = new float[3];
+    float m_colourTransitionTime = 2.5f;
+    
+    // used in calculations as '*' is faster than '/'
+    float m_colourTransitionRate;
+
+    // 0 = r -> g, 1 = g -> b, 2 = b -> r
+    int m_transitionStage = 0;
+
+    Material m_mat;
+
+    void Start()
+    {
+        m_colourTransitionRate = 1.0f / m_colourTransitionTime;
+
+        m_colours[0] = 1.0f;
+        m_colours[1] = 0.0f;
+        m_colours[2] = 0.0f;
+
+        m_mat = GetComponent<Renderer>().material;
+        m_mat.color = new Color(m_colours[0], m_colours[1], m_colours[2], 1.0f);
+    }
+    
+    void Update()
+    {
+        float dt = Time.deltaTime;
+
+        if (m_transitionStage == 0)
+        {
+            m_colours[0] -= m_colourTransitionRate * dt;
+            m_colours[1] += m_colourTransitionRate * dt;
+
+            if (m_colours[0] <= 0.0f)
+            {
+                m_colours[0] = 0.0f;
+                m_colours[1] = 1.0f;
+
+                m_transitionStage++;
+            }
+        }
+        else if (m_transitionStage == 1)
+        {
+            m_colours[1] -= m_colourTransitionRate * dt;
+            m_colours[2] += m_colourTransitionRate * dt;
+
+            if (m_colours[1] <= 0.0f)
+            {
+                m_colours[1] = 0.0f;
+                m_colours[2] = 1.0f;
+
+                m_transitionStage++;
+            }
+        }
+        else if (m_transitionStage == 2)
+        {
+            m_colours[2] -= m_colourTransitionRate * dt;
+            m_colours[0] += m_colourTransitionRate * dt;
+
+            if (m_colours[2] <= 0.0f)
+            {
+                m_colours[2] = 0.0f;
+                m_colours[0] = 1.0f;
+
+                m_transitionStage = 0;
+            }
+        }
+
+        m_mat.color = new Color(m_colours[0], m_colours[1], m_colours[2], 1.0f);
+    }
+}

4b63fa8f50d93253ac3bd1b23ceef5aeebaf6e08
Marcus Callender <m.shute@outlook.com>
2017-05-03 16:22:56 +0100

    Found a better solution for players pulling blocks while standing on top of them

--- The Puzzler/Assets/GameAssets/Code/BoxMovenemt.cs
+++ The Puzzler/Assets/GameAssets/Code/BoxMovenemt.cs
@@ -27,15 +27,22 @@ public class BoxMovenemt : MonoBehaviour
 
         float distance = Mathf.Abs(gameObject.transform.position.x - m_player.gameObject.transform.position.x);
 
-        if (distance > m_playerBoxMinDistance && distance < m_playerBoxMinDistance + m_playerInteractDistance)
+        if (/*distance > m_playerBoxMinDistance &&*/ distance < m_playerBoxMinDistance + m_playerInteractDistance)
         {
-            m_player.m_closeToBox = true;
+            float top = m_rigb.transform.position.y + (m_rigb.transform.localScale.y * 0.5f);
+            float bottom = m_rigb.transform.position.y - (m_rigb.transform.localScale.y * 0.5f);
 
-            Debug.Log("Player Close");
-
-            if (m_player.m_moveingBox)
+            if (m_playerRigb.transform.position.y > bottom && m_playerRigb.transform.position.y < top)
             {
-                m_rigb.velocity = new Vector3(m_playerRigb.velocity.x, m_rigb.velocity.y);
+                m_player.m_closeToBox = true;
+
+                Debug.Log("Player Close");
+
+                if (m_player.m_moveingBox)
+                {
+                    m_rigb.velocity = new Vector3(m_playerRigb.velocity.x, m_rigb.velocity.y);
+                }
+
             }
         }
     }

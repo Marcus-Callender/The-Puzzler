d8f5cb6cab1474d43a622b9acd47d243de605055
Marcus Callender <m.shute@outlook.com>
2017-05-18 20:50:22 +0100

    Started setup of ghost mechanic and updated to new unity version

---
+++ The Puzzler/Assets/GameAssets/Code/GhostData.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum E_INPUTS
+{
+    LEFT,
+    RIGHT,
+    JUMP,
+    //MOVE_BOX,
+    //PRESS_BUTTON,
+
+    NULL
+}
+
+public class GhostData : MonoBehaviour
+{
+    private char[] m_recordedInputs = new char[60 * 3];
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0.0f)
+        {
+
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0.0f)
+        {
+
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+
+        }
+    }
+
+    int InputToBit(E_INPUTS input)
+    {
+        int bit = 1;
+
+        for (int z = 0; z < (int)input; z++)
+        {
+            bit *= 2;
+        }
+
+        return bit;
+    }
+}

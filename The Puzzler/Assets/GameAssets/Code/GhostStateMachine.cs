554fe0f3142d83ab717cc352ca4d87f28cc1b014
Marcus Callender <m.shute@outlook.com>
2017-05-22 15:26:15 +0100

    Fixed a bug where activating the ghost broke the game

--- The Puzzler/Assets/GameAssets/Code/GhostStateMachine.cs
+++ The Puzzler/Assets/GameAssets/Code/GhostStateMachine.cs
@@ -50,9 +50,11 @@ public class GhostStateMachine : BaseStateMachine
 
     public override void Update()
     {
+        m_inputs.Cycle();
+
         base.Update();
 
-        if (m_inputs.m_recorded && m_inputs.m_arrayPosition == GhostInputs.m_recordingSize)
+        if (m_inputs.m_recorded && m_inputs.m_arrayPosition == GhostInputs.m_recordingSize - 1)
         {
             // runs if the recording has finished and the ghost is not playing
             m_data.m_anim.SetBool("Stopped", true);

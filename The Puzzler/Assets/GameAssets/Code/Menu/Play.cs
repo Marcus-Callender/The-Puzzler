369db1f48bf5fab4f52a5a26d073ee9ec2be4807
Marcus Callender <m.shute@outlook.com>
2017-07-15 12:37:51 +0100

    Made the play button take the player to the demo level

---
+++ The Puzzler/Assets/GameAssets/Code/Menu/Play.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public string m_sceneToLoad;
+
+    void Start()
+    {
+        UnityEngine.UI.Button btn = gameObject.GetComponent<UnityEngine.UI.Button>();
+        btn.onClick.AddListener(PlayGame);
+    }
+
+    void PlayGame()
+    {
+        SceneManager.LoadScene(m_sceneToLoad, LoadSceneMode.Single);
+    }
+}

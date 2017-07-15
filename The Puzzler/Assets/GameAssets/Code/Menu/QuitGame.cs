7d4cbc5e5fc9c5d3d61d53114e142a70c4d5b774
Marcus Callender <m.shute@outlook.com>
2017-07-15 12:29:12 +0100

    Added a main menu with a functioning quit button

---
+++ The Puzzler/Assets/GameAssets/Code/Menu/QuitGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour
+{
+    void Start()
+    {
+        UnityEngine.UI.Button btn = gameObject.GetComponent<UnityEngine.UI.Button>();
+        btn.onClick.AddListener(Quit);
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}

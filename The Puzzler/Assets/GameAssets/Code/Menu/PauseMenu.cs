10ec63f7f1ac82475c88a6a9b4000206190b0312
Marcus Callender <m.shute@outlook.com>
2017-07-16 18:01:20 +0100

    (WIP) added a pause menu to the game
    buttons currently dont work as needed

---
+++ The Puzzler/Assets/GameAssets/Code/Menu/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool m_paused = false;
+    private Transform m_menu;
+
+    void Start()
+    {
+        m_menu = transform.Find("Canvas");
+        m_menu.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            m_paused = !m_paused;
+
+            if (m_paused)
+            {
+                m_menu.gameObject.SetActive(true);
+                Time.timeScale = 0.0f;
+            }
+            else
+            {
+                m_menu.gameObject.SetActive(false);
+                Time.timeScale = 1.0f;
+            }
+        }
+    }
+}

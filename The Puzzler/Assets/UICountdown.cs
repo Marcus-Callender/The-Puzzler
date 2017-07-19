5ad024ca6b25f0b1c57e6b4f6148043d723c57c5
Marcus Callender <m.shute@outlook.com>
2017-07-19 14:29:41 +0100

    Added a demo progress timer

---
+++ The Puzzler/Assets/UICountdown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class UICountdown : MonoBehaviour
+{
+    Image m_sprite;
+    public float m_progress = 1.0f;
+
+    void Start()
+    {
+        m_sprite = GetComponent<Image>();
+
+        m_sprite.type = Image.Type.Filled;
+        m_sprite.fillMethod = Image.FillMethod.Radial360;
+        m_sprite.fillAmount = 1.0f;
+        m_sprite.fillClockwise = false;
+    }
+    
+    void Update()
+    {
+        m_sprite.fillAmount = m_progress;
+    }
+}

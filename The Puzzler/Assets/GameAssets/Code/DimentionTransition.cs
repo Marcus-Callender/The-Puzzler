72ac298842413c14ff591a94fca0d4c47e587320
Marcus Callender <m.shute@outlook.com>
2017-08-05 19:56:53 +0100

    Made the player align with a dimention transition so they are garented to be on a plain where they can interact with object properly

--- The Puzzler/Assets/GameAssets/Code/DimentionTransition.cs
+++ The Puzzler/Assets/GameAssets/Code/DimentionTransition.cs
@@ -29,6 +29,17 @@ public class DimentionTransition : MonoBehaviour
 
             if (Vector3.Angle(transform.right, other.transform.position - transform.position) < 90.0f)
             {
+                if (data.m_use3D)
+                {
+                    Vector3 newPos = Vector3.zero;
+
+                    newPos = transform.position;
+                    newPos.y = data.transform.position.y /*+ Mathf.Epsilon*/;
+                    newPos += -transform.forward * (transform.localScale.x * 2.0f);
+
+                    data.transform.position = newPos;
+                }
+
                 data.m_use3D = false;
                 data.m_left_right = m_camera_left_right;
                 data.SetRotation(gameObject.transform.rotation);

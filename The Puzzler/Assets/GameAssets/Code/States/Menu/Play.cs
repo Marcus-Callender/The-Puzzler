ffb5d865f6916bd394a2806d0216e0c7928c7345
Marcus Callender <m.shute@outlook.com>
2017-08-06 19:22:15 +0100

    Added sound effects to the main menu buttons

--- The Puzzler/Assets/GameAssets/Code/States/Menu/Play.cs
+++ The Puzzler/Assets/GameAssets/Code/States/Menu/Play.cs
@@ -8,15 +8,27 @@ using UnityEngine.SceneManagement;
 public class Play : MonoBehaviour
 {
     public string m_sceneToLoad;
+    public AudioSource m_audio;
 
     void Start()
     {
         UnityEngine.UI.Button btn = gameObject.GetComponent<UnityEngine.UI.Button>();
-        btn.onClick.AddListener(PlayGame);
+        btn.onClick.AddListener(StartPlayGame);
+        m_audio = GetComponent<AudioSource>();
     }
 
-    void PlayGame()
+    void StartPlayGame()
     {
+        StartCoroutine(PlayGame());
+    }
+
+    IEnumerator PlayGame()
+    {
+        m_audio.Play();
+        //m_audio.PlayOneShot(m_audio.clip, 1.0f);
+
+        yield return new WaitForSeconds(0.15f);
+
         SceneManager.LoadScene(m_sceneToLoad, LoadSceneMode.Single);
     }
 }
--- The Puzzler/Assets/GameAssets/Code/States/Menu/QuitGame.cs
+++ The Puzzler/Assets/GameAssets/Code/States/Menu/QuitGame.cs
@@ -6,14 +6,26 @@ using UnityEngine.UI;
 
 public class QuitGame : MonoBehaviour
 {
+    public AudioSource m_audio;
+
     void Start()
     {
         UnityEngine.UI.Button btn = gameObject.GetComponent<UnityEngine.UI.Button>();
-        btn.onClick.AddListener(Quit);
+        btn.onClick.AddListener(StartQuit);
+        m_audio = GetComponent<AudioSource>();
     }
 
-    void Quit()
+    void StartQuit()
     {
+        StartCoroutine(Quit());
+    }
+
+    IEnumerator Quit()
+    {
+        m_audio.Play();
+
+        yield return new WaitForSeconds(0.15f);
+
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else

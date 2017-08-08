a51a44e5396568ee6943b56178f81724f2c5d77d
Marcus Callender <m.shute@outlook.com>
2017-08-08 18:31:45 +0100

    Added a reset button to the main menu that deletes the save files

---
+++ The Puzzler/Assets/GameAssets/Code/ResetData.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
+
+using System;
+using System.IO;
+
+public class ResetData : MonoBehaviour
+{
+    public AudioSource m_audio;
+
+    void Start()
+    {
+        UnityEngine.UI.Button btn = gameObject.GetComponent<UnityEngine.UI.Button>();
+        btn.onClick.AddListener(StartReset);
+        m_audio = GetComponent<AudioSource>();
+    }
+
+    void StartReset()
+    {
+        StartCoroutine(Reset());
+    }
+
+    IEnumerator Reset()
+    {
+        m_audio.Play();
+
+        yield return new WaitForSeconds(0.15f);
+
+        string directory = Directory.GetCurrentDirectory();
+        
+        string saveDir = directory + "\\save.sav";
+        string posDir = directory + "\\posSave.sav";
+
+        if (File.Exists(saveDir))
+        {
+            File.Delete(saveDir);
+        }
+
+        if (File.Exists(posDir))
+        {
+            File.Delete(posDir);
+        }
+    }
+}

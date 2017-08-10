c2fc8e3d1146d3a50a79c11c45752d40c51cfbfa
Marcus Callender <m.shute@outlook.com>
2017-08-10 09:51:53 +0100

    Implemented the reset data confirmation screen and build code to transition between menu screens

--- The Puzzler/Assets/GameAssets/Code/Menus/ResetData.cs
+++ The Puzzler/Assets/GameAssets/Code/Menus/ResetData.cs
@@ -10,25 +10,14 @@ using System.IO;
 
 public class ResetData : MonoBehaviour
 {
-    public AudioSource m_audio;
-
     void Start()
     {
         UnityEngine.UI.Button btn = gameObject.GetComponent<UnityEngine.UI.Button>();
-        btn.onClick.AddListener(StartReset);
-        m_audio = GetComponent<AudioSource>();
+        btn.onClick.AddListener(DeleteFiles);
     }
-
-    void StartReset()
+    
+    void DeleteFiles()
     {
-        StartCoroutine(Reset());
-    }
-
-    IEnumerator Reset()
-    {
-        m_audio.Play();
-
-        yield return new WaitForSeconds(0.15f);
 
         string directory = Directory.GetCurrentDirectory();
         
--- The Puzzler/Assets/GameAssets/Code/Menus/SwitchMenuScreen.cs
+++ The Puzzler/Assets/GameAssets/Code/Menus/SwitchMenuScreen.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchMenuScreen : MonoBehaviour {
+public class SwitchMenuScreen : MonoBehaviour
+{
+    public GameObject m_nextMenu;
+    public AudioSource m_audio;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        UnityEngine.UI.Button btn = gameObject.GetComponent<UnityEngine.UI.Button>();
+        btn.onClick.AddListener(Switch);
+        m_audio = GetComponent<AudioSource>();
+
+    }
+    
+    void Switch()
+    {
+        m_audio.Play();
+
+        if (m_nextMenu)
+        {
+            m_nextMenu.SetActive(true);
+
+            GameObject myScreen = gameObject;
+
+            while (myScreen.tag != "MenuScreen")
+            {
+                myScreen = myScreen.transform.parent.gameObject;
+            }
+
+            myScreen.SetActive(false);
+        }
+    }
 }
